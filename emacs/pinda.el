fb7f17d9f8b71f30c4bf1ab461d147507475ffef
Joeri Djojosoeparto <joeri@breadandpepper.com>
2014-04-03 08:58:44 +0200

    Resolve conflict

--- emacs/pinda.el
+++ emacs/pinda.el
@@ -49,15 +49,7 @@
 (require 'pinda-theme)      ; set the theme and font
 (require 'pinda-temp)       ; temporary files
 (require 'pinda-shell)      ; shell mode
-<<<<<<< HEAD
-<<<<<<< HEAD
-;(require 'pinda-lisp)       ; lisp languages
-=======
 (require 'pinda-lisp)       ; lisp languages
->>>>>>> cc8f3dad1a77e4bd5c9e27d9c6647667736d5d49
-=======
-(require 'pinda-lisp)       ; lisp languages
->>>>>>> cc8f3dad1a77e4bd5c9e27d9c6647667736d5d49
 (require 'pinda-python)     ; python
 (require 'pinda-evil)       ; vim emulation
 (require 'pinda-cocoa)      ; cocoa
