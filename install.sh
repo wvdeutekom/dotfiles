74751c027caa60c255a4d485f188992891c00d92
Wijnand van Deutekom <wijnandvandeutekom@T1000.local>
2017-04-13 23:52:36 +0200

    Added spacemacs git clone to install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-#set -eo
 
 # Install script for my dotfiles
 # Advanced construction techniques (duct tape.)
@@ -68,6 +67,7 @@ install_spacemacs () {
     brew tap d12frosted/emacs-plus
     brew install emacs-plus --with-cocoa --with-gnutls --with-librsvg --with-imagemagick --with-spacemacs-icon
     brew linkapps
+    git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 }
 
 install_tmuxinator () {
