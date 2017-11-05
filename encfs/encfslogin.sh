3f8c4d8fca598a8eae2cef77f2ff50c913363582
Wijnand van Deutekom <wijnand@vdeute.com>
2017-11-05 22:06:50 +0100

    Fixed encfs script paths

--- encfs/encfslogin.sh
+++ encfs/encfslogin.sh
@@ -8,5 +8,5 @@ function get_pw() {
   security 2>&1 >/dev/null find-generic-password -ga "$1" | head -1 | sed 's/password: "\(.*\)"/\1/'
 }
 
-get_pw "encfs" | ENCFS6_CONFIG="$HOME/Sync/crypt/encfs6.xml" "$ENCFS" -S "$HOME/Sync/crypt" "$HOME/crypt"
+get_pw "encfs" | ENCFS6_CONFIG="$HOME/.crypt/encfs6.xml" "$ENCFS" -S "$HOME/.crypt" "$HOME/crypt"
 get_pw "encfs_photos" | ENCFS6_CONFIG="$HOME/Pictures/.picture_crypt/encfs6.xml" "$ENCFS" -S "$HOME/Pictures/.picture_crypt" "$HOME/Pictures/personal"
