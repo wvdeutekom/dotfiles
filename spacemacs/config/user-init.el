995167c3c252cf29be12b2597614dbcae944cc19
Wijnand van Deutekom <wijnand@vdeute.com>
2017-11-05 21:25:54 +0100

    Updated gitignore, emacs config

--- spacemacs/config/user-init.el
+++ spacemacs/config/user-init.el
@@ -3,4 +3,25 @@
 It is called immediately after `dotspacemacs/init'.  You are free to put almost
 any user code here.  The exception is org related code, which should be placed
 in `dotspacemacs/user-config'."
+
+  (setq golden-ratio-mode t
+        helm-follow-mode-persistent t
+        global-linum-mode t
+        projectile-enable-caching t
+        require-final-newline t
+        mode-require-final-newline t
+        )
+
+  (defun copy-from-osx ()
+    (shell-command-to-string "pbpaste"))
+
+  (defun paste-to-osx (text &optional push)
+    (let ((process-connection-type nil))
+      (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+        (process-send-string proc text)
+        (process-send-eof proc))))
+
+  (setq interprogram-cut-function 'paste-to-osx)
+  (setq interprogram-paste-function 'copy-from-osx)
+
   )
